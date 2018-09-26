018d03e2228fa6bac9fca2bc72ced718c9ddfda8
borg323 <borg323@users.noreply.github.com>
2018-09-26 16:20:29 +0300

    set CL_HPP_TARGET_OPENCL_VERSION to 110

--- src/neural/opencl/OpenCL.h
+++ src/neural/opencl/OpenCL.h
@@ -21,7 +21,7 @@
 using net_t = float;
 
 #define CL_HPP_MINIMUM_OPENCL_VERSION 110
-#define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_HPP_TARGET_OPENCL_VERSION 110
 #define CL_HPP_ENABLE_EXCEPTIONS
 #include <cstddef>
 #include <memory>
