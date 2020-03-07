df5b1d5c36657234f0fc9448b197f076a2d63516
borg323 <borg323@users.noreply.github.com>
2020-03-07 21:49:04 +0200

    use new embedding method

--- scripts/appveyor_android_package.cmd
+++ scripts/appveyor_android_package.cmd
@@ -2,9 +2,9 @@ git clone https://github.com/lealgo/chessenginesupport-androidlib.git --branch l
 cd oex
 git checkout 87e0168310c72dd059d9f574f4434f7f32de1214
 cd ..
-7z a -mx=0 embed.zip c:\cache\591226.pb.gz
-copy /y /b arm64-v8a\lc0+embed.zip oex\LeelaChessEngine\leelaChessEngine\src\main\jniLibs\arm64-v8a\liblc0.so
-copy /y /b armeabi-v7a\lc0+embed.zip oex\LeelaChessEngine\leelaChessEngine\src\main\jniLibs\armeabi-v7a\liblc0.so
+perl -e 'printf "%sLc0!", pack("V", -s "c:/cache/591226.pb.gz")' >tail.bin
+copy /y /b arm64-v8a\lc0+c:\cache\591226.pb.gz+tail.bin oex\LeelaChessEngine\leelaChessEngine\src\main\jniLibs\arm64-v8a\liblc0.so
+copy /y /b armeabi-v7a\lc0+c:\cache\591226.pb.gz+tail.bin oex\LeelaChessEngine\leelaChessEngine\src\main\jniLibs\armeabi-v7a\liblc0.so
 set ANDROID_HOME=C:\android-sdk-windows
 appveyor DownloadFile https://dl.google.com/android/repository/sdk-tools-windows-3859397.zip
 7z x sdk-tools-windows-3859397.zip -oC:\android-sdk-windows > nul
