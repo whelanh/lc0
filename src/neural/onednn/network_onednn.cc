964fb2f2124607f1d17dbc3a80e3d8f0e36d3867
borg323 <borg323@users.noreply.github.com>
2023-05-17 01:18:01 +0300

    more fine grained locking

--- src/neural/onednn/network_onednn.cc
+++ src/neural/onednn/network_onednn.cc
@@ -809,9 +809,10 @@ class OnednnNetwork : public Network {
       // Convert output data to nchw and if on gpu move them to the cpu.
       dnnl::memory opPol_mem_cpu;
       dnnl::memory opVal_mem_cpu;
-      std::lock_guard<std::mutex> lock(lock_);
+
       if (opPol_desc != opPol_mem.get_desc() ||
           eng_.get_kind() != dnnl::engine::kind::cpu) {
+        std::lock_guard<std::mutex> lock(lock_);
         opPol_mem_cpu = dnnl::memory(opPol_desc, cpu_eng_);
         dnnl::reorder pol_reorder = dnnl::reorder(opPol_mem, opPol_mem_cpu);
         pol_reorder.execute(eng_stream_, opPol_mem, opPol_mem_cpu);
@@ -821,6 +822,7 @@ class OnednnNetwork : public Network {
 
       if (opVal_desc != opVal_mem.get_desc() ||
           eng_.get_kind() != dnnl::engine::kind::cpu) {
+        std::lock_guard<std::mutex> lock(lock_);
         opVal_mem_cpu = dnnl::memory(opVal_desc, cpu_eng_);
         dnnl::reorder val_reorder_ = dnnl::reorder(opVal_mem, opVal_mem_cpu);
         val_reorder_.execute(eng_stream_, opVal_mem, opVal_mem_cpu);
@@ -830,6 +832,7 @@ class OnednnNetwork : public Network {
 
       if (moves_left_) {
         // MLH doesn't need post-processing so move directly to output buffer.
+        std::lock_guard<std::mutex> lock(lock_);
         dnnl::memory opMov_mem_cpu =
             dnnl::memory(opMov_desc, cpu_eng_, io->op_moves_left_mem_ + start);
         dnnl::reorder mov_reorder_ = dnnl::reorder(opMov_mem, opMov_mem_cpu);
